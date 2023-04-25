d22b48402dc0476f320f6d397aa9c78c151ec2df
baklyNik <baklykov.nikita369@gmail.com>
2023-04-25 11:31:04 +0300

    Initial commit

---
+++ src/main/java/com/example/stupek/StupekApplication.java
@@ -0,0 +1,13 @@
+package com.example.stupek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StupekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StupekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stupek/StupekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stupek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StupekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

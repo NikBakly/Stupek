ee87598d49149339f878807d2fe76e5e73454397
baklyNik <baklykov.nikita369@gmail.com>
2023-05-05 00:49:16 +0300

    add CourseController

--- src/main/java/com/example/stupek/course/CourseController.java
+++ src/main/java/com/example/stupek/course/CourseController.java
@@ -1,7 +1,38 @@
 package com.example.stupek.course;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/course")
 public class CourseController {
+    private final CourseService courseService;
+
+    @PostMapping
+    public CourseDto createCourse(@RequestBody CourseDto courseDto) {
+        return courseService.save(courseDto);
+    }
+
+    @PatchMapping("/{courseId}")
+    public CourseDto updateById(@PathVariable Long courseId, @RequestBody CourseDto courseDto) {
+        return courseService.updateById(courseId, courseDto);
+    }
+
+    @GetMapping("/{courseId}")
+    public CourseDto findById(@PathVariable Long courseId) {
+        return courseService.findById(courseId);
+    }
+
+    @GetMapping
+    public List<CourseDto> findAll(@RequestParam("offset") Integer offset, @RequestParam("limit") Integer limit) {
+        return courseService.findAll(offset, limit);
+    }
+
+    @DeleteMapping("/{courseId}")
+    public void deleteById(@PathVariable Long courseId) {
+        courseService.deleteById(courseId);
+    }
 }

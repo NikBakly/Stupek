cf927469afab85ee405ab7f42e04b9ea9612d02c
baklyNik <baklykov.nikita369@gmail.com>
2023-05-05 00:24:56 +0300

    add valid annotation for course dto

--- src/main/java/com/example/stupek/course/CourseDto.java
+++ src/main/java/com/example/stupek/course/CourseDto.java
@@ -1,17 +1,22 @@
 package com.example.stupek.course;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class CourseDto {
+    @NotBlank
     private String name;
 
     private String description;
 
+    @NotBlank
     private String material;
 
+    @Min(0)
     private Float price;
 
     private Boolean isOpen;
